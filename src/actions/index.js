958066402de0269b801c0f3c07096ac7c7122ba0
jihye kang <jihye59kang@gmail.com>
2021-07-15 21:31:47 +0100

    unique user ids

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,12 @@ import _ from 'lodash';
 
 import jsonplaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPostsAndUsers = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts())
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'))
+
+    userIds.forEach(id => dispatch(fetchUser(id)))
 }
 
 export const fetchPosts = () => async (dispatch) => {
