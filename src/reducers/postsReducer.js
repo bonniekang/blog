b708ec8138959f0efa1c432d4e06964897511a81
jihye kang <jihye59kang@gmail.com>
2021-07-15 15:25:52 +0100

    switch case in reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,6 @@
-export default () => {
-    return 123;
+export default (state = [], action) => {
+    switch(action.type) {
+        case 'FETCH_POSTS' :
+            return action.payload;
+    }
 }
